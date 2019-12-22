22414e43b608e15cedf7767850ee5e686a9fd970
Garrett Braatz <grbraatz@gmail.com>
2019-12-21 18:50:44 -0800

    Update test framework to AVA

--- test/spec.js
+++ test/spec.js
@@ -1,29 +1,40 @@
-const Application = require('spectron').Application
-const assert = require('assert')
-const electronPath = require('electron')
-const path = require('path')
+import test from 'ava';
 
-describe('Application Launch', function() {
-    this.timeout(10000)
+test('foo', t => {
+    t.pass();
+});
 
-    beforeEach(function() {
-        this.app = new Application({
-            path: electronPath,
-            args: [path.join(__dirname, '..')]
-        })
-        return this.app.start()
-    })
+test('bar', async t => {
+    const bar = Promise.resolve('bar');
+    t.is(await bar, 'bar');
+});
 
-    afterEach(function() {
-        if (this.app && this.app.isRunning()) {
-            return this.app.stop()
-        }
-    })
+// const Application = require('spectron').Application
+// const assert = require('assert')
+// const electronPath = require('electron')
+// const path = require('path')
 
-    it('shows an initial window', function () {
-        return this.app.client.getWindowCount().then(function (count) {
-            assert.equal(count, 2)
-            // getWindowCount() returns 2 if dev tools are opened.
-        })
-    })
-})
+// describe('Application Launch', function() {
+//     this.timeout(10000)
+
+//     beforeEach(function() {
+//         this.app = new Application({
+//             path: electronPath,
+//             args: [path.join(__dirname, '..')]
+//         })
+//         return this.app.start()
+//     })
+
+//     afterEach(function() {
+//         if (this.app && this.app.isRunning()) {
+//             return this.app.stop()
+//         }
+//     })
+
+//     it('shows an initial window', function () {
+//         return this.app.client.getWindowCount().then(function (count) {
+//             assert.equal(count, 1)
+//             // getWindowCount() returns 2 if dev tools are opened.
+//         })
+//     })
+// })
