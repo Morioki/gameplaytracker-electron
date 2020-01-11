9b962ec4863dbd57eecf28e688f2a9293d187073
Garrett Braatz <grbraatz@gmail.com>
2020-01-11 08:57:02 -0800

    Refactor stub data page

--- src/stub-data.js
+++ src/stub-data.js
@@ -27,25 +27,25 @@ module.exports = {
 	],
 	playSessionList: [
 		{
-			playSessionId: 1,
-			gameId: 1,
+			play_session_id: 1,
+			game_id: 1,
 			user: 1,
-			startDate: '2019-01-01 11:23:01',
-			endDate: '2019-01-01 13:43:22'
+			start_date: '2019-01-01 11:23:01',
+			end_date: '2019-01-01 13:43:22'
 		},
 		{
-			playSessionId: 2,
-			gameId: 1,
+			play_session_id: 2,
+			game_id: 1,
 			user: 1,
-			startDate: '2019-12-12 09:32:01',
-			endDate: '2019-12-12 10:14:42'
+			start_date: '2019-12-12 09:32:01',
+			end_date: '2019-12-12 10:14:42'
 		},
 		{
-			playSessionId: 3,
-			gameId: 2,
+			play_session_id: 3,
+			game_id: 2,
 			user: 1,
-			startDate: '2018-09-02 02:05:25',
-			endDate: '2018-09-02 05:23:08'
+			start_date: '2018-09-02 02:05:25',
+			end_date: '2018-09-02 05:23:08'
 		}
 	]
 };
