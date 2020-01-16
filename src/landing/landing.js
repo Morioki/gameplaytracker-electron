547077ff0e1fe4095b85ebe7628a0119b4b57eca
Garrett Braatz <grbraatz@gmail.com>
2020-01-15 17:21:56 -0800

    Begin rebuild of Landing Page

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -1,19 +1,25 @@
 const {BrowserWindow} = require('electron').remote;
 const windowStateKeeper = require('electron-window-state');
+const Mousetrap = require('mousetrap');
 
 const dbConn = require('../db/db-connection');
 
 // DOM Nodes
-const playSession = document.querySelector('#play-session');
-const gameRecord = document.querySelector('#game-record');
-const gameList = document.querySelector('#game-list');
-const playSessionList = document.querySelector('#play-session-list');
-const dbTest = document.querySelector('#db-test');
+// const playSession = document.querySelector('#play-session');
+// const gameRecord = document.querySelector('#game-record');
+// const gameList = document.querySelector('#game-list');
+// const playSessionList = document.querySelector('#play-session-list');
+// const dbTest = document.querySelector('#db-test');
 
 // Load Session Data for all windows
 dbConn.loadAllData()
 
-gameList.addEventListener('click', async e => {
+// konami code!
+Mousetrap.bind('up up down down left right left right b a enter', () => {
+	console.log('konami code')
+  })
+
+Mousetrap.bind('g', () =>{
 	const gameListWindow = window.open('../game-list/static/game-list.html', '', `
 		maxWidth=2000,
 		maxHeight=2000,
@@ -22,9 +28,11 @@ gameList.addEventListener('click', async e => {
 		backgroundColor=#DEDEDE,
 		nodeIntegration=1
 	`);
+	
+	return false;
 });
 
-playSessionList.addEventListener('click', async e => {
+Mousetrap.bind('p', () => {
 	const playSessionListWindow = window.open('../play-session-list/static/play-session-list.html', '', `
 		maxWidth=2000,
 		maxHeight=2000,
@@ -32,5 +40,12 @@ playSessionList.addEventListener('click', async e => {
 		height=800,
 		backgroundColor=#DEDEDE,
 		nodeIntegration=1
-	`);
+	`);	
+	
+	return false;
 });
+
+// TODO Build UI for Landing Page
+//* Aggregate information. Top 5 games by hours played
+//* Game Name | Release Year | Hours Played 
+
